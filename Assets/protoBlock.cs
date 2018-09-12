da0977cb7cf59fe31ac1c85e171a9172f0687082
Unknown <hairpasta@hotmail.com>
2018-09-12 12:27:06 +0200

    [+] VRTK, Balance Prototype

---
+++ Assets/protoBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class protoBlock : MonoBehaviour {
+	private bool end;
+	void Start () {
+		transform.rotation = Random.rotation;
+	}
+
+	private void Update() {
+		if (!end && transform.position.y<.2f){
+			end=true;
+			Destroy(gameObject,3);
+		}
+	}
+	
+}
---
+++ Assets/protoSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class protoSpawner : MonoBehaviour {
+
+	public float radius = 3;
+	public float frequency = 2;
+	public GameObject[] blocks;
+	private float timer = 0;
+
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer>frequency){
+			SpawnBlock();
+			timer=0;
+		}
+	}
+
+	public void SpawnBlock(){
+		Vector3 pos = Random.insideUnitSphere*radius;
+		pos.y=0;
+		GameObject b = Instantiate(blocks[Random.Range(0,blocks.Length)],transform);
+		b.transform.position += pos;
+	}
+
+	private void OnDrawGizmos() {
+		Gizmos.DrawWireSphere(transform.position,radius);
+	}
+}
