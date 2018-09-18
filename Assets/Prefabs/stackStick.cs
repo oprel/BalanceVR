4db5c95ea5614681f3e8a2473edcbbe1455926f9
Jasper Oprel <jasperoprel@hotmail.com>
2018-09-19 01:00:47 +0200

    [+] proto version for keeping stack together, put .gitignore back

---
+++ Assets/Prefabs/stackStick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stackStick : MonoBehaviour {
+
+
+	public bool absoluteParent;
+	[HideInInspector] public bool partofStack;
+	private stackStick parent;
+	private Vector3 originalScale;
+	
+	private void Awake() {
+		originalScale = transform.localScale;
+	}
+
+	private void OnCollisionEnter(Collision col) {
+		if (partofStack) return;
+		stackStick other  = col.transform.GetComponent<stackStick>();
+		if (other && (other.partofStack || other.absoluteParent)){
+			partofStack = true;
+			parent = other;
+			//transform.parent = parent.transform;
+			transform.SetParent(other.transform,true);
+		}
+	}
+
+	
+	private void OnCollisionExit(Collision col) {
+		if (!partofStack) return;
+		stackStick other  = col.transform.GetComponent<stackStick>();
+		if (other && other == parent){
+			partofStack = false;
+			parent = null;
+			transform.parent=null;
+			transform.localScale=originalScale;
+		}
+	}
+}
