42e4ef74cb3e68a1278bd33f0bca1ae6e7daea36
Unknown <mickteun@gmail.com>
2018-09-19 16:30:49 +0200

    Prototype

---
+++ Assets/destroyFallenObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyFallenObject : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+            
+        }
+        
+
+
+
+
+
+    }
+    
+
+
+
+
+
+}
