ebd7bf008e67f141db43f3d6aacf7a65b45d1795
Unknown <mickteun@gmail.com>
2018-10-02 19:36:08 +0200

    Names

---
+++ Assets/giveName.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class giveName : MonoBehaviour {
+
+    public static string playernamestr1;
+    public Text playername1;
+
+    public static string playernamestr2;
+    public Text playername2;
+
+    public static string playernamestr3;
+    public Text playername3;
+
+    public static string playernamestr4;
+    public Text playername4;
+
+
+    // Use this for initialization
+    void Start () {
+
+        playername1.text = playernamestr1;
+        playername2.text = playernamestr2;
+        playername3.text = playernamestr3;
+        playername4.text = playernamestr4;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/mainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainMenu : MonoBehaviour {
+
+    public InputField playername1;
+    public InputField playername2;
+    public InputField playername3;
+    public InputField playername4;
+
+    private void Awake() {
+        
+    }
+
+    public void startLevel1() {
+
+        Debug.Log("Playername1 is: " + playername1.text);
+        Debug.Log("Playername2 is: " + playername2.text);
+        Debug.Log("Playername3 is: " + playername3.text);
+        Debug.Log("Playername4 is: " + playername4.text);
+
+        giveName.playernamestr1 = playername1.text;
+        giveName.playernamestr2 = playername2.text;
+        giveName.playernamestr3 = playername3.text;
+        giveName.playernamestr4 = playername4.text;
+
+        Application.LoadLevel(1);
+    }
+
+    
+
+}
+
