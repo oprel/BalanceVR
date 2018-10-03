26c3d9034ec16feec8109902fb1363379d94499b
Unknown <mickteun@gmail.com>
2018-10-03 20:50:03 +0200

    sound en wat assets rest volgt nog

---
+++ Assets/assetSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assetSound : MonoBehaviour {
+
+    private AudioSource AS;
+    public AudioClip blockSound;
+
+    void Start() {
+        AS = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision col) {
+       
+                AS.PlayOneShot(blockSound);   
+        
+    }
+}
+ 
